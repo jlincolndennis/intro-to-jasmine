76a298c7391ab80488467960142b10dea82d3590
Lincoln Dennis <j.lincoln.dennis@gmail.com>
2016-02-24 13:56:16 -0700

    Add Rides dummt test

---
+++ spec/rides_spec.js
@@ -0,0 +1,17 @@
+var rides = require('../lib/rides');
+
+describe('Rides', function () {
+  // it('is our master plan here', function () {
+  //   expect(rides.testFunction()).toEqual('running stuff for realsies.')
+  // });
+
+  it('determines when a child is too short', function() {
+    expect(.rides.isTallEnough()).toEqual(false)
+  })
+
+
+
+  it('determines when a child is tall enough', function() {
+    expect(.rides.isTallEnough()).toEqual(true)
+  })
+})
