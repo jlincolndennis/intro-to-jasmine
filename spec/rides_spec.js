3749307ed2cb49a7256f72814bb7ba41e2e1cd3b
Lincoln Dennis <j.lincoln.dennis@gmail.com>
2016-02-24 14:24:52 -0700

    All tests pass. Its a Whole New World of testing.

--- spec/rides_spec.js
+++ spec/rides_spec.js
@@ -1,17 +1,61 @@
 var rides = require('../lib/rides');
 
-describe('Rides', function () {
+describe('Rides', function() {
   // it('is our master plan here', function () {
   //   expect(rides.testFunction()).toEqual('running stuff for realsies.')
   // });
 
   it('determines when a child is too short', function() {
-    expect(.rides.isTallEnough()).toEqual(false)
-  })
+  var child1 = {
+    height: 3,
+    name: "Julian",
+    age: 3
+  };
+
+  var child2 = {
+    height: 4,
+    name: "Cero",
+    age: 4
+  };
+  var ride1 = {
+    minHeight: 3,
+    maxHeight: 7
+  };
+
+  var ride2 = {
+    minHeight: 4,
+    maxHeight: 7
+  };
+
+  expect(rides.isTallEnough(child1, ride2)).toEqual(false)
 
 
+})
 
   it('determines when a child is tall enough', function() {
-    expect(.rides.isTallEnough()).toEqual(true)
+    var child1 = {
+      height: 3,
+      name: "Julian",
+      age: 3
+    };
+
+    var child2 = {
+      height: 4,
+      name: "Cero",
+      age: 4
+    };
+    var ride1 = {
+      minHeight: 3,
+      maxHeight: 7
+    };
+
+    var ride2 = {
+      minHeight: 4,
+      maxHeight: 7
+    };
+    expect(rides.isTallEnough(child1, ride1)).toEqual(true)
+    expect(rides.isTallEnough(child2, ride1)).toEqual(true)
+    expect(rides.isTallEnough(child2, ride2)).toEqual(true)
+
   })
 })
